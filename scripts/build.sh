4e640c01cd1333d90904407c53ce6f942c241626
Nicolas Viennot <Nicolas.Viennot@twosigma.com>
2021-10-23 02:54:02 +0000

    scripts/build.sh accepts a --debug option

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 set -ex
 
+# Note: You may use --debug for building images faster when running tests
+
 SCRIPT_DIR=$(dirname -- "$(readlink -f -- "$0")")
 cd "$SCRIPT_DIR"/..
 
-docker build -f scripts/Dockerfile.build . -t fastfreeze-build
+BUILD_ENV=()
+CARGO_OPTS=()
+if [ "$1" == '--debug' ]; then
+    BUILD_ENV=("--build-arg" "BUILD=debug")
+    CARGO_OPTS=("--build-arg" "CARGO_OPTS=")
+fi
+
+docker build -f scripts/Dockerfile.build . -t fastfreeze-build "${BUILD_ENV[@]}" "${CARGO_OPTS[@]}"
 docker run fastfreeze-build cat /src/fastfreeze/fastfreeze.tar.xz > ./fastfreeze.tar.xz
-docker build -f scripts/Dockerfile.test . -t fastfreeze-test
+docker build -f scripts/Dockerfile.test . -t fastfreeze-test "${BUILD_ENV[@]}"
 docker build -f scripts/Dockerfile.test.ff_installed . -t fastfreeze-test-ff-installed
